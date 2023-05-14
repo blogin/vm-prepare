5736fbc4112e2429ac3d6d63a0a833782b0e062f
fix <alogin2009@gmail.com>
2023-05-14 05:41:14 -0700

    ubuntu 20.04

---
+++ ubuntu-20.04.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update && apt install -y git locate htop apt-transport-https ca-certificates curl software-properties-common wget
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce
+sudo usermod -aG docker fix
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
